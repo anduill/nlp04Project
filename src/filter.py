6a75e21d308ee38594da06cba5074ffccc5b3e57
Czar <czar@Aldebaran.(none)>
2014-04-09 21:28:46 -0500

    added parameters to filter.py

--- src/filter.py
+++ src/filter.py
@@ -22,11 +22,16 @@ def traverse_dir(root):
     else:
         h5 = hdf5_getters.open_h5_file_read(root)
         tags = hdf5_getters.get_artist_mbtags(h5)
+        print tags.keys()
         for genre in genreDict.keys():
-             if genre in tags:
-                 genreDict[genre].append(root)
+            if genre in tags:
+                genreDict[genre].append(root)
         h5.close()
-genreDict = {'rock':[],'pop':[],'country':[], 'hip-hop':[], 'punk':[]}
-print [len(genreDict[x]) for x in genreDict.keys()]
-traverse_dir(sys.argv[1])
-print [len(genreDict[x]) for x in genreDict.keys()]
+        
+dataPath = sys.argv[1]
+minGenreSize = int(sys.argv[2])
+maxGenreSize = int(sys.argv[3])
+genreKeys = [sys.argv[i+4] for i in range(len(sys.argv)-4)]
+genreDict = {x:[] for x in genreKeys}
+
+traverse_dir(dataPath)
