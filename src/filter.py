075574e23d2c8aae9bb5456fd11ebbf2e65280ec
David J. Garcia <djchuy@Davids-MacBook-Pro.local>
2014-04-19 12:11:50 -0500

    changed filter.py to be more modular

--- src/filter.py
+++ src/filter.py
@@ -3,20 +3,25 @@ Created on Apr 8, 2014
 
 @author: czar
 '''
-from os import listdir, makedirs
-from os.path import isfile, isdir, exists
-import h5py
+import os
+from os import listdir
+from os.path import isfile
 import hdf5_getters
-import matplotlib.pyplot as plt
-import numpy as np
 import sys
-import json
 
-def traverse_dir(root):
+
+def notValidSong(tags, artist, songName, segmentTimbre, segmentPitches):
+    return tags.size ==0 or not artist or not songName or segmentTimbre.size == 0 or segmentPitches.size ==0
+
+def genreInTags(genre,tags):
+    for tag in tags:
+        if genre in tag:
+            return True
+
+def traverseAndWrite(root, genreFileDescriptors):
     if not isfile(root):
         for f in listdir(root):
-            # print root + "/" + f
-            traverse_dir(root + "/" + f)
+            traverseAndWrite(root + "/" + f,genreFileDescriptors)
     else:
         h5 = hdf5_getters.open_h5_file_read(root)
         numOfSongs = hdf5_getters.get_num_songs(h5)
@@ -27,45 +32,45 @@ def traverse_dir(root):
             songName = hdf5_getters.get_title(h5,index)
             segmentTimbre = hdf5_getters.get_segments_timbre(h5,index)
             segmentPitches = hdf5_getters.get_segments_pitches(h5,index)
-            if tags.size ==0 or not artist or not songName or segmentTimbre.size == 0 or segmentPitches.size ==0:
+            if notValidSong(tags, artist, songName, segmentTimbre, segmentPitches):
                 h5.close()
                 continue
-            for genre in genreDict.keys():
-                if len(genreDict[genre]) == maxGenreSize:
-                    continue
-                if genre in tags:
-                    genreDict[genre].append((genre,artist, songName, segmentPitches.tolist(), segmentTimbre.tolist()))
+            for genre in genreFileDescriptors.keys():
+                if genreInTags(genre,tags):
+                    song = {}
+                    song['genre'] = genre
+                    song['artist_name'] = artist
+                    song['song_title'] = songName
+                    song['segments_pitches'] = segmentPitches.tolist()
+                    song['segments_timbre'] = segmentTimbre.tolist()
+                    line = str(song)
+                    writeToDescriptor(genre,genreFileDescriptors,line)
         h5.close()
-        
-def write():
-    makedirs(outputFileName)
-    for genre in genreDict.keys():
-        data = [ { 'genre':x[0], 'artist_name':x[1], 'song_title':x[2], 'segments_pitches':x[3],'segments_timbre':x[4] } for x in genreDict[genre]]
-        if len(data) < minGenreSize:
-            continue
-        with open(outputFileName+"/"+genre+".json", 'w') as outfile:
-            json.dump(data, outfile, sort_keys = True, indent = 4)
-            
-if len(sys.argv) < 6:
-    print "ERROR - Parameters Missing"
-    print "  <dataPath> <outputPath> <minGenreSize> <maxGenreSize> <genre>+"
-    print "  MSC/DATA/PATH/ OUTPUT/PATH 1 10 pop country bachata"
-    sys.exit(0)
-dataPath = sys.argv[1]
-outputFileName = sys.argv[2]
-minGenreSize = int(sys.argv[3])
-maxGenreSize = int(sys.argv[4])
-genreKeys = [sys.argv[i + 5] for i in range(len(sys.argv) - 5)]
-genreDict = {x:[] for x in genreKeys}
 
-traverse_dir(dataPath)
-write()
+
+def writeToDescriptor(genreKey, genreDict, line):
+    fd = genreDict[genreKey]
+    fd.write(line)
+    fd.write("\n")
 
 def main(sys_args):
+    if len(sys_args) < 6:
+        print "ERROR - Parameters Missing"
+        print "  <dataPath> <outputPath> <minGenreSize> <maxGenreSize> <genre>+"
+        print "  MSC/DATA/PATH/ OUTPUT/PATH 1 10 pop country bachata"
+        sys.exit(0)
     dataPath = sys_args[1]
     output_dir = sys_args[2]
     minGenreSize = int(sys_args[3])
     maxGenreSize = int(sys_args[4])
+    genreKeys = [sys.argv[i + 5] for i in range(len(sys.argv) - 5)]
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    genreFileDescriptors = {gk:open(output_dir+"/"+gk+".json",'w') for gk in genreKeys}
+
+    traverseAndWrite(dataPath, genreFileDescriptors)
+    for key in genreFileDescriptors.keys():
+        genreFileDescriptors[key].close()
 
 if __name__ == "__main__":
     main(sys.argv)
